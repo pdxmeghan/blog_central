df5deeb5f2eb466c85f65f2a0168361542caf25c
Meghan Lindsley <pdxmeghan@gmail.com>
2014-09-11 19:30:14 -0700

    complete CRUD and views for Entry model.

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,55 @@
+class EntriesController < ApplicationController
+  def index
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @entry = Entry.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @entry = Entry.new(entry_params)
+    if @entry.save
+      session[:entry_id] = @entry.id
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @entry = Entry.find(params[:id])
+    @user = User.find(params[:user_id])
+  end
+
+  def edit
+    @entry = Entry.find(params[:id])
+    @user = User.find(params[:user_id])
+  end
+
+  def update
+    @entry = Entry.find(params[:id])
+    @user = User.find(params[:user_id])
+    @entry.update(entry_params)
+    if @user.valid?
+      flash[:notice] = "#{@entry.title} has been updated"
+      redirect_to user_entry_path(@user, @entry)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @entry = Entry.find(params[:id])
+    @user = User.find(params[:user_id])
+    @entry.destroy
+    flash[:notice] = "#{@entry.title} has been deleted"
+    redirect_to user_path(@user)
+  end
+
+private
+  def entry_params
+    params.require(:entry).permit(:title, :content).merge(:user_id => current_user.id)
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @entries = Entry.where(:user_id == @user.id)
   end
 
   def edit
