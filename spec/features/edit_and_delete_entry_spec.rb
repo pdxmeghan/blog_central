15ae1d9d8200246ab9733e25c3e455f6d3669ecf
Meghan Lindsley <pdxmeghan@gmail.com>
2014-09-15 19:22:00 -0700

    passin specs for editing and deleting a blog entry

--- spec/features/edit_and_delete_entry_spec.rb
+++ spec/features/edit_and_delete_entry_spec.rb
@@ -19,4 +19,43 @@ describe "editing and deleting a blog entry" do
     fill_in 'Content', :with => 'oh yeah'
     expect(page).to have_content "oh yeah"
   end
+
+  it "will not edit a blog entry with blank fields" do
+    user = User.create!(:name => "Friend", :password => '123')
+    visit '/login'
+    fill_in 'Name', :with => 'Friend'
+    fill_in 'password', :with => '123'
+    click_button 'Log In'
+    click_link "#{user.name}"
+    click_link 'Create New Entry'
+    fill_in 'Title', :with => 'hey'
+    fill_in 'Content', :with => 'yo'
+    click_button 'Publish Entry'
+    entry = Entry.create!(:title => 'hey', :content => 'yo')
+    visit "/users/#{user.id}"
+    click_link 'hey'
+    click_link 'Edit Entry'
+    fill_in 'Content', :with => ''
+    visit "/users/#{user.id}/entries/#{entry.id}"
+    expect(page).to have_content "yo"
+  end
+
+  it "will delete a blog entry" do
+    user = User.create!(:name => "Friend", :password => '123')
+    visit '/login'
+    fill_in 'Name', :with => 'Friend'
+    fill_in 'password', :with => '123'
+    click_button 'Log In'
+    click_link "#{user.name}"
+    click_link 'Create New Entry'
+    fill_in 'Title', :with => 'hey'
+    fill_in 'Content', :with => 'yo'
+    click_button 'Publish Entry'
+    entry = Entry.create!(:title => 'hey', :content => 'yo')
+    visit "/users/#{user.id}"
+    click_link 'hey'
+    click_link 'Delete Entry'
+    visit "/users/#{user.id}"
+    expect(page).to have_content " "
+  end
 end
